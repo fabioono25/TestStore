b4ca2a79a8c8144430fa71e732517af5182b6db6
Fabio Ono Tavoloni <fabioono25@gmail.com>
2019-12-29 10:38:02 +0000

    Implementando a camada de aplicacao

---
+++ src/TestStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TestStore.Catalogo.Application.DtosViewModels;
+using TestStore.Catalogo.Domain;
+
+namespace TestStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
---
+++ src/TestStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using TestStore.Catalogo.Application.DtosViewModels;
+using TestStore.Catalogo.Domain;
+
+namespace TestStore.Catalogo.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(p =>
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro,
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
